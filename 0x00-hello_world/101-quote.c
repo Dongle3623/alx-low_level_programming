dbad171a5ec0c2cced9c8cd3b29271ec690bf041
Dongle3623 <ymokhtar695@gmail.com>
2023-03-16 19:40:23 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <unistd.h>
-
+/**
+ * main - prints a string to standard error
+ *
+ * Return: 1
+ */
 int main(void)
 {
 char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
