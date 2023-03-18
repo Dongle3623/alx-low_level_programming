0cf5044a6cc87ee1ceb784196b52f427e61b948d
Dongle3623 <ymokhtar695@gmail.com>
2023-03-17 17:32:08 -0700

    task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - Prints the alphabet in lowercase except for q and e using only putchar
+ * main - Prints the alphabet in lowercase 
+ * except for q and e using only putchar
  *
  * Return: Always 0 (Success)
  */
