4458ebef2853f4eba2deb17daf13a1404967d82f
Dongle3623 <ymokhtar695@gmail.com>
2023-03-28 17:16:41 -0700

    task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,6 @@
 /**
- * _strcpy - copies the string pointed to by src to the buffer pointed to by dest
+ * _strcpy - copies the string pointed to by src to
+ * the buffer pointed to by dest
  * @dest: pointer to the destination buffer
  * @src: pointer to the source string
  *
