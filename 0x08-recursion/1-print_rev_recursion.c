22cf14b3ffa06a5db7a659c86da78be092691ec5
Dongle3623 <ymokhtar695@gmail.com>
2023-09-28 16:57:00 -0700

    test

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,7 +3,7 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  *
- * Description: This function prints each character of the string `s` in reverse
+ * Description: This function prints each character of the string `s`in reverse
  * order using recursion. It starts by checking if the current character is the
  * null character (`'\0'`). If it is, the function returns. Otherwise, it calls
  * itself with the next character in the string (`s + 1`) and then prints the
