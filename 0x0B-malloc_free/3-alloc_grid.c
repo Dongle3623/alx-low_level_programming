4e4c84fced8e415c2cf3eacea549bf03bd59ed72
Dongle3623 <ymokhtar695@gmail.com>
2023-10-04 14:36:07 -0700

    task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,21 +7,24 @@
  *
  * Return: Pointer to the newly allocated grid (NULL on failure)
  */
-int **alloc_grid(int width, int height) {
+int **alloc_grid(int width, int height)
+{
 int **grid;
 int i, j;
 if (width <= 0 || height <= 0)
-return NULL;
+return (NULL);
 grid = malloc(height * sizeof(int *));
 if (grid == NULL)
-return NULL;
-for (i = 0; i < height; i++) {
+return (NULL);
+for (i = 0; i < height; i++)
+{
 grid[i] = malloc(width * sizeof(int));
-if (grid[i] == NULL) {
+if (grid[i] == NULL)
+{
 for (; i >= 0; i--)
 free(grid[i]);
 free(grid);
-return NULL;
+return (NULL);
 }
 for (j = 0; j < width; j++)
 grid[i][j] = 0;
