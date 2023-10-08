0f3f693da95a22bc96f72dc06c52b318e3738d32
Dongle3623 <ymokhtar695@gmail.com>
2023-10-08 16:36:29 -0700

    task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,9 +15,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 void *ptr;
 if (nmemb == 0 || size == 0)
 return NULL;
-ptr = malloc(nmemb * size);
+ptr = malloc(nmemb *size);
 if (ptr == NULL)
 return (NULL);
-memset(ptr, 0, nmemb * size);
+memset(ptr, 0, nmemb *size);
 return (ptr);
 }
