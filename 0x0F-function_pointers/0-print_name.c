5ddf78ca895d366500ff6e1620371079182ba8b9
Dongle3623 <ymokhtar695@gmail.com>
2023-10-11 14:39:30 -0700

    task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print - Prints a string
+ * @str: The string to print
+ */
+void print(char *str)
+{
+    while (*str)
+    {
+        write(1, str, 1);
+        str++;
+    }
+    write(1, "\n", 1);
+}
+
+/**
+ * print_name - Prints a name using a function pointer
+ * @name: The name to print
+ * @f: The function to use to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_name("Betty", print);
+    return (0);
+}
